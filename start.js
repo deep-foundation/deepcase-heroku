128a8077b9f986129add92ad8bd09d30d57574d7
Пётр Безденежных <menzorg@gmail.com>
2021-10-01 23:55:36 +0300

    wait-on

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 const { spawn, execSync } = require('child_process');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+var waitOn = require('wait-on');
 var express = require('express');
 var app = express();
 
@@ -55,16 +56,31 @@ deeplinksApp.on('close', (code) => {
   console.log(`deeplinksApp exited with code ${code}`);
 });
 
-let migrations;
-setTimeout(()=>{
+var migrations;
+var opts = {
+  resources: [
+    'tcp:localhost:8080',
+    'tcp:localhost:3007',
+  ],
+  delay: 500, // initial delay in ms, default 0
+  interval: 100, // poll interval in ms, default 250ms
+  simultaneous: 1, // limit to 1 connection per resource at a time
+  timeout: 30000, // timeout in ms, default Infinity
+  tcpTimeout: 1000, // tcp timeout in ms, default 300ms
+};
+
+waitOn(opts, function (err) {
+  if (err) {
+    console.log(`{ "logtype": "migrations", "log": "err"`);
+  }
   migrations = spawn('npm', ['run', 'migrate']);
   deeplinksApp.stderr.on('data', (data) => {
     console.log(`{ "logtype": "migrations", "error": ${data}`);
   });
   migrations.stdout.on('data', (data) => {
-   console.log(`{ "logtype": "migrations", "log": "${data}""`);
+   console.log(`{ "logtype": "migrations", "log": "${data}"`);
   });
   migrations.on('close', (code) => {
     console.log(`migrations exited with code ${code}`);
   });
-}, 20000);
+});
