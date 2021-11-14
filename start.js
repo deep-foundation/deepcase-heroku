5cbea0090fa4adc87e6f9b86ef4caf12ca356e3f
Пётр Безденежных <menzorg@gmail.com>
2021-11-15 01:45:06 +0300

    migrate with delay

--- start.js
+++ start.js
@@ -9,7 +9,7 @@ app.use('/hasura', createProxyMiddleware({
   changeOrigin: true,
   ws: true,
   pathRewrite: {
-    "^/hasura": "/",
+    "/hasura": "/",
   }
 }));
 app.use('/', createProxyMiddleware({ target: `http://localhost:3007`, changeOrigin: true }));
@@ -63,25 +63,25 @@ var opts = {
     'tcp:localhost:8080',
     'tcp:localhost:3007',
   ],
-  delay: 500, // initial delay in ms, default 0
+  delay: 2000, // initial delay in ms, default 0
   interval: 100, // poll interval in ms, default 250ms
   simultaneous: 1, // limit to 1 connection per resource at a time
   timeout: 30000, // timeout in ms, default Infinity
   tcpTimeout: 1000, // tcp timeout in ms, default 300ms
 };
 
-// waitOn(opts, function (err) {
-//   if (err) {
-//     console.log(`{ "logtype": "migrations", "log": "err"`);
-//   }
-//   migrations = spawn('npm', ['run', 'migrate']);
-//   deeplinksApp.stderr.on('data', (data) => {
-//     console.log(`{ "logtype": "migrations", "error": ${data}`);
-//   });
-//   migrations.stdout.on('data', (data) => {
-//    console.log(`{ "logtype": "migrations", "log": "${data}"`);
-//   });
-//   migrations.on('close', (code) => {
-//     console.log(`migrations exited with code ${code}`);
-//   });
-// });
+waitOn(opts, function (err) {
+  if (err) {
+    console.log(`{ "logtype": "migrations", "log": "err"`);
+  }
+  migrations = spawn('npm', ['run', 'migrate']);
+  deeplinksApp.stderr.on('data', (data) => {
+    console.log(`{ "logtype": "migrations", "error": ${data}`);
+  });
+  migrations.stdout.on('data', (data) => {
+   console.log(`{ "logtype": "migrations", "log": "${data}"`);
+  });
+  migrations.on('close', (code) => {
+    console.log(`migrations exited with code ${code}`);
+  });
+});
